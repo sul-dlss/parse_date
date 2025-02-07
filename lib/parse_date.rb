877189d979ad5c2b693d89a6ce4b5b82e47b9f1c
Michael J. Giarlo <mjgiarlo@stanford.edu>
2025-02-07 12:19:04 -0800

    Remove misleading usage of `protected` keyword
    And refactor labyrinthine, overly explicit structure.

--- lib/parse_date.rb
+++ lib/parse_date.rb
@@ -23,31 +23,18 @@ class ParseDate
   class Error < StandardError; end
 
   include Singleton
-  extend ParseDate::IntFromString
-
-  # class method delegation
-  def self.earliest_year(date_str)
-    ParseDate::IntFromString.earliest_year(date_str)
-  end
-
-  def self.latest_year(date_str)
-    ParseDate::IntFromString.latest_year(date_str)
-  end
-
-  def self.year_int_valid?(date_str)
-    ParseDate::IntFromString.year_int_valid?(date_str)
-  end
+  extend IntFromString
 
   # @return [Array] array of Integer year values from earliest year to latest year, inclusive
   def self.parse_range(date_str)
     first = earliest_year(date_str)
     last = latest_year(date_str)
     return nil unless first || last
-    raise ParseDate::Error, "Unable to parse range from '#{date_str}'" unless year_range_valid?(first, last)
+    raise Error, "Unable to parse range from '#{date_str}'" unless year_range_valid?(first, last)
 
     range_array(first, last)
   rescue StandardError => e
-    raise ParseDate::Error, "Unable to parse range from '#{date_str}': #{e.message}"
+    raise Error, "Unable to parse range from '#{date_str}': #{e.message}"
   end
 
   # true if:
@@ -65,20 +52,16 @@ class ParseDate
   # @param [Integer, String] first_year, expecting integer or parseable string for .to_i
   # @param [Integer, String] last_year, expecting integer or parseable string for .to_i
   # @return [Array] array of Integer year values from first to last, inclusive
-  # rubocop:disable Metrics/CyclomaticComplexity
-  # rubocop:disable Metrics/PerceivedComplexity
-  def self.range_array(first_year, last_year)
+  def self.range_array(first_year, last_year) # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
     first_year = first_year.to_i if first_year.is_a?(String) && first_year.match?(/^-?\d+$/)
     last_year = last_year.to_i if last_year.is_a?(String) && last_year.match?(/^-?\d+$/)
 
     return [] unless last_year || first_year
     return [first_year] if last_year.nil? && first_year
     return [last_year] if first_year.nil? && last_year
-    raise(ParseDate::Error, "unable to create year range array from #{first_year}, #{last_year}") unless
+    raise(Error, "unable to create year range array from #{first_year}, #{last_year}") unless
       year_range_valid?(first_year, last_year)
 
     Range.new(first_year, last_year).to_a
   end
-  # rubocop:enable Metrics/CyclomaticComplexity
-  # rubocop:enable Metrics/PerceivedComplexity
 end
